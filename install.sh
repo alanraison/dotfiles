a44b9b5a661d8cde6249dbe35763f9d83c7b803e
Alan Raison <alan.raison@hitachicapital.co.uk>
2020-05-15 08:33:40 +0000

    Basic Powerline configuration

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh}/themes/powerlevel10k
 
 install zshrc ~/.zshrc
+install p10k.zsh ~/.p10k.zsh
